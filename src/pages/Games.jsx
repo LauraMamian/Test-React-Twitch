3c2e70d08beeb8612ce0857f19a3d194eb67c859
LauraMamian <lauramamianc@unicauca.edu.co>
2022-10-01 00:29:27 -0500

    TOP games

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function Games() {
   return (
-    <div className='animate__animated animate__fadeIn fixed z-50 top-0 w-full min-h-screen flex justify-center items-center'>
-      <div className='animate__animated animate__fadeInDown z-50 w-3/4 md:w-1/4 bg-gray-900 rounded-lg'>
-
-      </div>
+    <div className="w-11/12 min-h-screen flex justify-center items-center md:justify-start">
+      
     </div>
   )
 }
